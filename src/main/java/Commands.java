53ad6325418f4d89edbd6332688a961dd94d848f
balashov <sashabalashov2019@gmail.com>
2023-10-18 11:12:07 +0500

    Added jokes that Shaposhnikov wrote.  Sources in the Shaposhnikov branch

--- src/main/java/Commands.java
+++ src/main/java/Commands.java
@@ -1,13 +1,18 @@
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import java.util.Random;
+import java.util.List;
 
 public class Commands {
     public static String start(String request){
         StringBuilder stringBuilder = new StringBuilder();
+        Random random = new Random();
+        int jokeCount = random.nextInt(10);
         switch (request) {
             case ("/weather"):
                 return ("Пока в разработке");
             case ("/joke"):
-                return("Пока в разработке");
+                List<String> jokes = Reader.read("C:\\Users\\HP\\Desktop\\Java\\OOP-v2\\src\\joke.txt");
+                return jokes.get(jokeCount);
             case ("/wikipedia"):
                 return("Пока в разработке");
             case ("/game"):
